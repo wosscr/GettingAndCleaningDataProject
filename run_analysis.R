3d5cb3f71a6a7e2f354c84295124a157fdb347df
Walter <wosscr@gmail.com>
2014-05-25 18:22:26 -0400

    Fixed version
    A lot of corrections since the last version, the merge function was
    creating problems when adding the activity names

--- run_analysis.R
+++ run_analysis.R
@@ -1,111 +1,79 @@
-## run_analysis.R
-
-## This script assumes that the zip file was uncompressed in the data directory and that the folder name hasn't changed
-
-# necessary libraries
+# load required libraries
 library("plyr")
 library("reshape2")
 
-######## load the data into R ######## 
 
-#label text, it works for both training and testing sets
-activityLabels <- read.table("./data/UCI HAR Dataset/activity_labels.txt")
-activityLabels[ , "V2"] <- tolower(activityLabels$V2)
-activityLabels[ , "V2"] <- sub("_", " ", activityLabels$V2)
+# load training set
+trainingSetData <- read.table("./data/UCI HAR Dataset/train/X_train.txt")
 
+# load test set
+testSetData <- read.table("./data/UCI HAR Dataset/test/X_test.txt")
+
+# merge data sets
+mergedData <- rbind(trainingSetData, testSetData)
+
+# load label ids for the training set
+trainingActivityIds <- read.table("./data/UCI HAR Dataset/train/Y_train.txt")
+names(trainingActivityIds) <- c("activity")
+
+# label label ids for the test set
+testActivityIds <- read.table("./data/UCI HAR Dataset/test/Y_test.txt")
+names(testActivityIds) <- c("activity")
+
+# append sets of activities
+activities <- rbind(trainingActivityIds, testActivityIds)
+
+# load the subjects for the training set
+trainingSubjects <- read.table("./data/UCI HAR Dataset/train/subject_train.txt")
+names(trainingSubjects) <- c("subject")
+
+# load the subjects for the test set
+testSubjects <- read.table("./data/UCI HAR Dataset/test/subject_test.txt")
+names(testSubjects) <- c("subject")
+
+# append sets of subjects
+subjects <- rbind(trainingSubjects, testSubjects)
+
+# append subject and activities data to the merged data
+mergedData <- cbind(subject = subjects$subject, mergedData)
+mergedData <- cbind(activity = activities$activity, mergedData)
+
+# load the feature names
 featureLabels <- read.table("./data/UCI HAR Dataset/features.txt")
 names(featureLabels) <- c("id", "feature")
 featureLabels <- rbind(data.frame(id= 0, feature = "subject"), featureLabels)
 featureLabels <- rbind(data.frame(id= 0, feature = "activity"), featureLabels)
 
-##### Training Set ##### 
-
-### labels ###
-# label id for training set
-trainingActivityIds <- read.table("./data/UCI HAR Dataset/train/Y_train.txt")
-# merge label names with the ids
-trainingActivity <- merge(activityLabels, trainingActivityIds, by = "V1", all = TRUE)
-# rename columns
-names(trainingActivity) <- c("id", "activity")
-
-### subjects ###
-# training subjects
-trainingSubjects <- read.table("./data/UCI HAR Dataset/train/subject_train.txt")
-# rename coolumns
-names(trainingSubjects) <- c("subject")
-
-### data ###
-# training set
-trainingSetData <- read.table("./data/UCI HAR Dataset/train/X_train.txt")
-
-# add activities and subjects to the data
-trainingSet <- cbind(subject = trainingSubjects$subject, trainingSetData)
-trainingSet <- cbind(activity = trainingActivity$activity, trainingSet)
-
-# remove unnecesary items from memory
-rm("trainingActivityIds","trainingActivity","trainingSetData","trainingSubjects")
-
-
-
-
-##### Testing Set #####
-
-### labels ###
-# label id for test set
-testActivityIds <- read.table("./data/UCI HAR Dataset/test/Y_test.txt")
-# merge label names with the ids
-testActivity <- merge(activityLabels, testActivityIds, by = "V1", all = TRUE)
-# rename columns
-names(testActivity) <- c("id", "activity")
-
-### subjects ###
-# training subjects
-testSubjects <- read.table("./data/UCI HAR Dataset/test/subject_test.txt")
-# rename coolumns
-names(testSubjects) <- c("subject")
-
-### data ###
-# test set
-testSetData <- read.table("./data/UCI HAR Dataset/test/X_test.txt")
-
-# add activities and subjects to the data
-testSet <- cbind(subject = testSubjects$subject, testSetData)
-testSet <- cbind(activity = testActivity$activity, testSet)
-
-# remove unnecesary items from memory
-rm("testActivityIds","testActivity","testSetData","testSubjects","activityLabels")
-
-##### Merge and Analyzing Data #####
-## 1. Merges the training and the test sets to create one data set
-mergedData <- merge(trainingSet, testSet, all = TRUE)
-
+# update the names of the data set with the featuer names
 names(mergedData) <- featureLabels$feature
 
-rm("testSet", "trainingSet")
+# load the activity names
+activityLabels <- read.table("./data/UCI HAR Dataset/activity_labels.txt")
+activityLabels[ , "V2"] <- tolower(activityLabels$V2)
+activityLabels[ , "V2"] <- sub("_", " ", activityLabels$V2)
+names(activityLabels) <- c("activity", "activitynames")
 
-## 2. Extracts only the measurements on the mean and standard deviation for each measurement
+activityNames <- join(activities, activityLabels, by = "activity")
+
+# append activity names to the data set
+mergedData <- cbind(activityNames, mergedData)
+
+# create the list of variables that need to be extracted
 meanFeatures <- featureLabels[grepl("mean()",featureLabels$feature), ]
 meanFeatures <- meanFeatures[!grepl("meanFreq()",meanFeatures$feature), ]
 stdFeatures <- featureLabels[grepl("std()",featureLabels$feature), ]
 selectedFeaturesNames <- merge(meanFeatures, stdFeatures, all = TRUE)
 selectedFeaturesNames <- rbind(data.frame(id = 0, feature = "subject"), selectedFeaturesNames)
-selectedFeaturesNames <- rbind(data.frame(id = 0, feature = "activity"), selectedFeaturesNames)
+selectedFeaturesNames <- rbind(data.frame(id = 0, feature = "activitynames"), selectedFeaturesNames)
 
+# createa a data frame with the extrated variables
 selectedFeatures <- mergedData[, which(names(mergedData) %in% selectedFeaturesNames$feature)]
 
-rm("meanFeatures", "selectedFeaturesNames", "stdFeatures", "featureLabels")
+# reshape the data based on activity names and subject
+shapedData <- melt(selectedFeatures, id.vars = .(activitynames, subject))
 
-## 3. Uses descriptive activity names to name the activities in the data set
-# this is already done by adding the column names to the set
+# get the medians per subject and activity
+shapedDataMedians <- dcast(shapedData, activitynames + subject ~ variable, mean)
 
-## 4. Appropriately labels the data set with descriptive activity names.
-# this is already done by converting activities to small and replacing underscore with 
-# blank spaces since the beginning before the activities were bound to the data set
-
-## 5. Creates a second, independent tidy data set with the average of each variable for each activity and each subject. 
-# I couldn't find a way to return the average per column grouped, I could only return the column average in general
-# even though is grouped, all fields have the same value which is the aveage for the whole column
-newSet <- ddply(selectedFeatures, .(activity, subject), function(x) colwise(mean)(selectedFeatures[ , c(-1, -2)]))
-
-# write the data to the data set
-write.table(newSet, file = "./data/tidyData.txt")
+# write the result in a file as result
+write.table(shapedDataMedians, file = "./data/tidyData.txt")
